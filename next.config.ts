887a25adc0dd31ddbb6b3e8aa3ef6c3a699ae325
aceknoxe <harigovindes@outlook.com>
2025-02-20 10:06:49 +0530

    initial a

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,6 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   experimental: {
-    optimizeCss: true,
     turbo: {
       rules: {
         "*.svg": ['@svgr/webpack'],
